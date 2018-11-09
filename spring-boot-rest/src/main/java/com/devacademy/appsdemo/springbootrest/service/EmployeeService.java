887340905a8baabb98ea8084b7f35fa3e56d526f
sanjivss1989 <sanjivss1989@gmail.com>
2018-11-09 16:41:22 +0530

    Revert "Spring Boot CRUD"
    This reverts commit f75b9568d484c861bf8d375e697b09e9858223e4.
    
    Revert code

--- spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/service/EmployeeService.java
+++ spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/service/EmployeeService.java
@@ -3,29 +3,10 @@
  */
 package com.devacademy.appsdemo.springbootrest.service;
 
-import java.util.List;
-
-import com.devacademy.appsdemo.springbootrest.model.Employee;
-
 /**
  * @author server
  *
  */
 public interface EmployeeService {
-	
-	public void saveEmployee(Employee emp);
-	
-	public List<Employee> retrieveEmployees();
-	
-	
-	public Employee getEmployee(Long id);
-	
-	
-	public void updateEmployee(Employee emp);
-	
-	
-	public void deleteEmployee(Long id);
-	
-	
 
 }
--- spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/service/impl/EmployeeServiceImpl.java
+++ spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/service/impl/EmployeeServiceImpl.java
@@ -3,60 +3,12 @@
  */
 package com.devacademy.appsdemo.springbootrest.service.impl;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.devacademy.appsdemo.springbootrest.model.Employee;
-import com.devacademy.appsdemo.springbootrest.repository.EmployeeRepository;
 import com.devacademy.appsdemo.springbootrest.service.EmployeeService;
 
 /**
  * @author server
  *
  */
-@Service
 public class EmployeeServiceImpl implements EmployeeService {
-	
-	@Autowired
-	EmployeeRepository empRepo;
-
-	@Override
-	public void saveEmployee(Employee emp) {
-		// TODO Auto-generated method stub
-		empRepo.save(emp);
-	}
-
-	@Override
-	public List<Employee> retrieveEmployees() {
-		// TODO Auto-generated method stub
-		List<Employee> eList = new ArrayList<Employee>();
-		eList = empRepo.findAll();
-		return eList;
-	}
-
-	@Override
-	public Employee getEmployee(Long id) {
-		// TODO Auto-generated method stub
-		Optional<Employee> optional = empRepo.findById(id);
-		Employee emp = optional.get();
-		return emp;
-		
-	}
-
-	@Override
-	public void updateEmployee(Employee emp) {
-		// TODO Auto-generated method stub
-		empRepo.save(emp);
-	}
-
-	@Override
-	public void deleteEmployee(Long id) {
-		// TODO Auto-generated method stub
-		empRepo.deleteById(id);
-	}
 
 }
