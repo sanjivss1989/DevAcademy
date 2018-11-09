673ef4a5792ef219889a6d87e15f8493817ff5ff
Sanjivsworld66 <sanjivsworld66@gmail.com>
2018-11-09 11:12:35 +0530

    First Commit Spring Boot Demo Project

---
+++ SpringBootDemo/src/main/java/com/devacademy/demo/controller/EmployeeController.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.devacademy.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devacademy.demo.model.Employee;
+
+/**
+ * @author server
+ *
+ */
+@RestController
+@ComponentScan("com.devacademy.demo")
+public class EmployeeController {
+	
+	@GetMapping(path = "/viewall")
+	public List<Employee> getEmployee()
+	{
+		List<Employee> eList = new ArrayList<Employee>();
+		eList.add(new Employee("8043652", "Sanjib", "sanjivsworld66@gmail.com"));
+		eList.add(new Employee("8043653", "John", "JohnyS66@gmail.com"));
+		return eList;
+		
+	}
+	
+	
+
+}
