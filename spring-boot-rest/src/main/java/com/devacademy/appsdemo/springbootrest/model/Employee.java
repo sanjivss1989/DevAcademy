6a9e50f32ca54fb415cc338ae894ebe211e4ec2f
sanjivss1989 <sanjivss1989@gmail.com>
2018-11-09 14:26:36 +0530

    Employee Model Object Creation

--- spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/model/Employee.java
+++ spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/model/Employee.java
@@ -3,10 +3,84 @@
  */
 package com.devacademy.appsdemo.springbootrest.model;
 
+import javax.annotation.Generated;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  * @author server
  *
  */
+@Entity
+@Table(name = "EMPLOYEE")
 public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "NAME")
+	private String name;
+    
+	@Column(name = "SALARY")
+	private Integer salary;
+	
+	@Column(name = "DEPARTMENT")
+	private String department;
+	
+	public Employee()
+	{
+		super();
+	}
+
+	public Employee(Long id, String name, Integer salary, String department) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+		this.department = department;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Integer salary) {
+		this.salary = salary;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	
+	
+	
+	
+	
 
 }
