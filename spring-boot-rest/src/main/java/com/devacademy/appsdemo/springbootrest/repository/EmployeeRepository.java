ec0abe1e673fd7241780e7d27212dbae0c2fa605
sanjivss1989 <sanjivss1989@gmail.com>
2018-11-09 18:55:21 +0530

    Pom.xml changes swagger addition

--- spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/repository/EmployeeRepository.java
+++ spring-boot-rest/src/main/java/com/devacademy/appsdemo/springbootrest/repository/EmployeeRepository.java
@@ -4,6 +4,7 @@
 package com.devacademy.appsdemo.springbootrest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.devacademy.appsdemo.springbootrest.model.Employee;
 
@@ -11,6 +12,7 @@ import com.devacademy.appsdemo.springbootrest.model.Employee;
  * @author server
  *
  */
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
 }
