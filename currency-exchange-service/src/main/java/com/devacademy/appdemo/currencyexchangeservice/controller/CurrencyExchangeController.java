953e2cec2c6e1143ec729254a4c7fc1c7d42fc42
sanjivss1989 <sanjivss1989@gmail.com>
2018-11-28 12:51:23 +0530

    Latest Changes

---
+++ currency-exchange-service/src/main/java/com/devacademy/appdemo/currencyexchangeservice/controller/CurrencyExchangeController.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.devacademy.appdemo.currencyexchangeservice.controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devacademy.appsdemo.currencyexchangeservice.bean.ExchangeValue;
+
+/**
+ * @author server
+ *
+ */
+@RestController
+public class CurrencyExchangeController {
+
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to)
+	{
+		return new ExchangeValue(1000L, from, to, BigDecimal.valueOf(65));
+	}
+	
+}
